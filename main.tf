4070eb3cbc499e08280f7fadbf8fb61b602fc0f6
Ben Fortuna <bfortuna@odecee.com.au>
2021-11-25 09:46:58 +1100

    Added build status badge

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@
  * # Terraform Module Template
  *
  * ![Terraform Module Template](terraform_module_template.png)
+ *
+ * ![main workflow](https://github.com/figurate/terraform-module-template/actions/workflows/main.yml/badge.svg)
  */
